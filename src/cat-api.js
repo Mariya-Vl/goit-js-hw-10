3a3cdf6d53b6b9810f5b993d1ae486bb5b7526f5
Mariia <mariya.s.vlasova@gmail.com>
2023-08-28 21:17:05 +0300

    Added loader. Edited mistakes

--- src/cat-api.js
+++ src/cat-api.js
@@ -6,47 +6,17 @@ axios.defaults.baseURL = 'https://api.thecatapi.com/v1';
 
 const ENDPOINT_BREEDS = '/breeds/';
 const ENDPOINT_CAT_SEARCH = '/images/search/';
-const loader = document.querySelector('.loader');
-const catInfoWrapp = document.querySelector('.cat-info');
 
 export function fetchBreeds() {
-  showLoader();
   return axios.get(`${ENDPOINT_BREEDS}`).then(response => {
-    if (!response.status == 200) {
-      throw new Error(response.message);
-    }
-    hideLoader();
     return response.data;
   });
 }
 
 export function fetchCatByBreed(breedId) {
-  hideInfo();
-  showLoader();
   return axios
     .get(`${ENDPOINT_CAT_SEARCH}?breed_ids=${breedId}`)
     .then(response => {
-      if (!response.status == 200) {
-        throw new Error(response.message);
-      }
-      showInfo();
-      hideLoader();
       return response;
     });
-}
-
-export function hideLoader() {
-  loader.setAttribute('hidden', 'hidden');
-}
-
-function showLoader() {
-  loader.removeAttribute('hidden');
-}
-
-function hideInfo() {
-  catInfoWrapp.setAttribute('hidden', 'hidden');
-}
-
-function showInfo() {
-  catInfoWrapp.removeAttribute('hidden');
 }
--- src/index.js
+++ src/index.js
@@ -1,43 +1,57 @@
-import { fetchBreeds, fetchCatByBreed, hideLoader } from './cat-api';
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
+import { Loading } from 'notiflix/build/notiflix-loading-aio';
 
 const refs = {
   selector: document.querySelector('.breed-select'),
   catInfoWrapp: document.querySelector('.cat-info'),
   error: document.querySelector('.error'),
+  loader: document.querySelector('.loader'),
+  firstOption: document.querySelector('[data-placeholder]'),
 };
+
+showLoader();
 
 fetchBreeds()
   .then(data => {
-    refs.error.setAttribute('hidden', 'hidden');
+    hideError();
+    hideLoader();
+    showSelect();
     return renderSelectorList(data);
   })
   .catch(() => {
-    hideLoader;
-    refs.error.removeAttribute('hidden');
+    hideLoader();
+    showError();
   });
 
-refs.error.setAttribute('hidden', 'hidden');
+hideError();
+
 function renderSelectorList(breedsList) {
   const markupSelector = breedsList
     .map(breed => `<option value="${breed.id}">${breed.name}</option>`)
     .join('');
-  refs.selector.innerHTML = markupSelector;
+  refs.selector.insertAdjacentHTML('beforeend', markupSelector);
+  refs.firstOption.setAttribute('disabled', 'disabled');
 }
 
 refs.selector.addEventListener('change', showCatsInfo);
 
 function showCatsInfo(evt) {
-  refs.error.setAttribute('hidden', 'hidden');
+  hideError();
+  showLoader();
   const breedId = evt.target.value;
   // console.log("ID породи: ",breedId);
   fetchCatByBreed(breedId)
     .then(response => {
+      hideInfo();
+      showLoader();
       return response;
     })
     .then(data => {
       return data.data[0];
     })
     .then(data => {
+      showInfo();
+      hideLoader();
       const descr = data.breeds[0].description;
       const name = data.breeds[0].name;
       const url = data.url;
@@ -49,7 +63,41 @@ function showCatsInfo(evt) {
       refs.catInfoWrapp.innerHTML = markupSelector;
     })
     .catch(() => {
-      hideLoader;
-      refs.error.removeAttribute('hidden');
+      hideLoader();
+      showError();
     });
+}
+
+function hideLoader() {
+  Loading.remove();
+  refs.loader.setAttribute('hidden', 'hidden');
+  refs.catInfoWrapp.removeAttribute('hidden');
+}
+
+function showLoader() {
+  Loading.dots('Loading data. Please wait...');
+  refs.catInfoWrapp.setAttribute('hidden', 'hidden');
+  refs.loader.removeAttribute('hidden');
+}
+
+function hideInfo() {
+  refs.catInfoWrapp.setAttribute('hidden', 'hidden');
+}
+
+function showInfo() {
+  refs.catInfoWrapp.removeAttribute('hidden');
+}
+
+function hideError() {
+  refs.error.setAttribute('hidden', 'hidden');
+  refs.catInfoWrapp.removeAttribute('hidden');
+}
+
+function showError() {
+  refs.catInfoWrapp.setAttribute('hidden', 'hidden');
+  refs.error.removeAttribute('hidden');
+}
+
+function showSelect() {
+  refs.selector.removeAttribute('hidden');
 }
